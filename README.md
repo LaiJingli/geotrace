c3587e05a20e24b47f97d206d1526574123b6853
laijingli <laijingli@github.com>
2015-01-29 11:39:29 +0800

    add support for linux and macos

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 geotrace是根据mtr（my trace route）的报告内容结合whois来直观显示traceroute过程中经过的路由跳数、ip、平均延迟、运营商、地理位置信息等内容，在做机房选址的时候会给带来极大的帮助，报告内容一目了然。
 
-运行系统：linux
+运行系统：linux/macos
 
 依赖程序：mtr、whois、awk
 
--- geotrace.sh
+++ geotrace.sh
@@ -3,8 +3,21 @@ echo traceroute to [$1] from localhost
 ip=$1
 echo
 
-####显示详细地理位置信息
-#mtr --n --report $ip|grep -v Snt|awk '{printf "%-18s  %-10s",  NR ") "$1,"  Delay["$4"s]   ";system("whois "$1"|grep -e netname -e descr|cut -c17-");printf "\n"}'
+####如果是linux系统请将mac部分注释掉，反之亦然
 
+####for linux
+####显示详细地理位置信息
+#echo ----显示详细地理位置信息----
+#mtr --n --report $ip|grep -v Snt|awk '{printf "%-18s  %-10s",  NR ") "$1,"  Delay["$4"s]   ";system("whois "$1"|grep -e netname -e descr|cut -c17-");printf "\n"}'
 ####显示简略地理位置信息
-mtr --n --report $ip|grep -v Snt|awk '{printf "%-18s  %-10s",  NR ") "$1,"  Delay["$4"s]   ";system("whois "$1"|grep descr|head -n1|cut -c17-");printf "\n"}'
+#echo ----示简略地理位置信息---
+#mtr --n --report $ip|grep -v Snt|awk '{printf "%-18s  %-10s",  NR ") "$1,"  Delay["$4"s]   ";system("whois "$1"|grep descr|head -n1|cut -c17-");printf "\n"}'
+
+
+###for mac
+####显示详细地理位置信息
+echo ----显示详细地理位置信息----
+mtr --n --report $ip|grep -vE "Snt|Start"|awk '{printf "%-18s  %-10s",  NR ") "$2, " Dleay["$6"]  ";system("whois "$2"|grep -e netname -e descr|cut -c17-");printf "\n"}'
+####显示简略地理位置信息
+echo ----示简略地理位置信息---
+mtr --n --report $ip|grep -vE "Snt|Start"|awk '{printf "%-18s  %-10s",  NR ") "$2, " Dleay["$6"]  ";system("whois "$2"|grep descr|head -n1|cut -c17-");printf "\n"}'
